7c25c13dcdbfabe51505c40185fae368bfb20cd0
nansu0425 <nansu0425@gmail.com>
2024-05-14 20:50:16 +0900

    Update include, libraries, macros
    Input/output, Containers, Thread, Windows, Common

--- src/lib-stateful-core/common/CommonPch.hpp
+++ src/lib-stateful-core/common/CommonPch.hpp
@@ -2,9 +2,34 @@
 
 #pragma once
 
-// C++ Standard Library headers
+// Input/output
 #include <iostream>
 
-// Common headers
+// Containers
+#include <array>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+// Thread
+#include <mutex>
+#include <atomic>
+
+// Windows
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+// Common
 #include <lib-stateful-core/common/CommonMacros.hpp>
 #include <lib-stateful-core/common/CommonTypes.hpp>
