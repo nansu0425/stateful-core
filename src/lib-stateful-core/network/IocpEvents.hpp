7ed34134ac8210373ddd7182488efde13a093d56
nansu0425 <nansu0425@gmail.com>
2024-05-20 00:29:43 +0900

    Added IocpEvents

--- src/lib-stateful-core/network/IocpEvents.hpp
+++ src/lib-stateful-core/network/IocpEvents.hpp
@@ -6,6 +6,9 @@ namespace StatefulCore
 {
 	namespace Network
 	{
+		class Session;
+		class SendBuffer;
+
 		enum class IocpEventType : byte
 		{
 			CONNECT,
@@ -15,6 +18,40 @@ namespace StatefulCore
 			SEND,
 		};
 
+		class ConnectEvent : public IocpEvent
+		{
+		public:
+			ConnectEvent() : IocpEvent(IocpEventType::CONNECT) { }
+		};
 
+		class DisconnectEvent : public IocpEvent
+		{
+		public:
+			DisconnectEvent() : IocpEvent(IocpEventType::DISCONNECT) { }
+		};
+
+		class AcceptEvent : public IocpEvent
+		{
+		public:
+			AcceptEvent() : IocpEvent(IocpEventType::ACCEPT) { }
+
+		private:
+			SPtr<Session>    m_session = nullptr;
+		};
+
+		class ReceiveEvent : public IocpEvent
+		{
+		public:
+			ReceiveEvent() : IocpEvent(IocpEventType::RECEIVE) { }
+		};
+
+		class SendEvent : public IocpEvent
+		{
+		public:
+			SendEvent() : IocpEvent(IocpEventType::SEND) { }
+
+		private:
+			Memory::Vector<SPtr<SendBuffer>>     m_sendBufs;
+		};
 	}
 }
